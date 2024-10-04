e408f7b8b17b41f4d5e4e63823853246e3546177
AraujoMJ <marcio_ok66@hotmail.com>
2024-10-04 11:49:30 +0700

    Redo rename Family_Column in Data_Total

--- Thinning_BreedR.R
+++ Thinning_BreedR.R
@@ -94,9 +94,9 @@ Thinning_BreedR <- function(BV_Column = "a_total",
     arrange(desc(get(BV_Column))) |>
     mutate(Family = factor(Family, levels = Family))
   
-  # Rename target column to 'Family'
-  Data_Total <- Data_Total |> 
-    rename_with(~ "Family", all_of(Family_Column))
+  # # Rename target column to 'Family'
+  # Data_Total <- Data_Total |> 
+  #   rename_with(~ "Family", all_of(Family_Data_Total))
   
   # Families with positive BV
   MajorBV_fam <- BV_fam |>
